622e13518bb61d2fa5dae7a3b00020585c31dac9
t-momoka <0903kjp@gmail.com>
2025-01-20 01:14:00 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 async function showTanukis() {
     const zooId = document.getElementById('zoo-select').value;
-    const response = await fetch(`http://localhost:3000/api/tanuki?zoo_id=${zooId}`);
+    const response = await fetch(`https://test-g5o8.onrender.com/api/tanuki?zoo_id=${zooId}`);
     const tanukis = await response.json();
     const tanukiList = document.getElementById('tanuki-list');
     tanukiList.innerHTML = '';
